3aa7f553e5434b6c74f464b823185156829f6e90
8cd <mail@8cd.ru>
2016-11-11 04:10:25 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+﻿import socket                                                                   
+import os                                                                       
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
+s.bind(('0.0.0.0',2222))                                                        
+s.listen(1)                                                                     
+for i in range(10):                                                             
+        fid=os.fork()                                                           
+        if fid==0:                                                              
+                while True:                                                     
+                        conn, addr = s.accept()                                 
+                        while True:                                             
+                                data = conn.recv(1024)                          
+                                if (not data) or (data=='close'): break         
+                                conn.send(data)                                 
+                        conn.close()
